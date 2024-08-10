8a70ab12a65b39f1be556fc5d963815fdd4820e0
Snak <snak.hoshinonaka@gmail.com>
2024-08-10 10:48:48 +0900

    linterを修正する

--- api/settings.py
+++ api/settings.py
@@ -10,15 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-import os
 from pathlib import Path
+
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
-env.read_env(Path.joinpath(BASE_DIR, '.env'))
+env.read_env(Path.joinpath(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
